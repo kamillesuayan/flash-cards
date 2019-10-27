5429715b5f1860321d7929d8c03f2f6d25d1f12c
kamillesuayan <kamille.suayan@gmail.com>
2019-10-27 04:14:53 -0700

    Updates

---
+++ precache-manifest.64073441deb0e247fbe01d2fed1c0258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae19f89f599742b32a05abf1fa6e313",
+    "url": "/flash-cards/index.html"
+  },
+  {
+    "revision": "9c7e36244d59fd4b1beb",
+    "url": "/flash-cards/static/css/main.de0ee2ef.chunk.css"
+  },
+  {
+    "revision": "d1891f01a6264784b0ce",
+    "url": "/flash-cards/static/js/2.a63a7da7.chunk.js"
+  },
+  {
+    "revision": "9c7e36244d59fd4b1beb",
+    "url": "/flash-cards/static/js/main.e7e0edc6.chunk.js"
+  },
+  {
+    "revision": "8ff9a90bc9e259d057ba",
+    "url": "/flash-cards/static/js/runtime-main.268a2f33.js"
+  }
+]);
